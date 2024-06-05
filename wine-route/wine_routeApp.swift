526ccd1a00d3874bde00b983e5538262f2ac16f9
상은 <71699054+vmkmym@users.noreply.github.com>
2024-06-05 13:17:31 +0900

    Initial Commit

---
+++ wine-route/wine_routeApp.swift
@@ -0,0 +1,17 @@
+//
+//  wine_routeApp.swift
+//  wine-route
+//
+//  Created by MorinBeck on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct wine_routeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
